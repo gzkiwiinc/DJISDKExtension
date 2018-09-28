b7a155ed70a6f02e4ed80dec8c66f920075e1965
Kyle <lacklock@gmail.com>
2018-09-28 11:38:11 +0800

    refactor: refactor rotate gimbal func

--- DJISDKExtension/Promise/DJIGimbal+Promise.swift
+++ DJISDKExtension/Promise/DJIGimbal+Promise.swift
@@ -13,9 +13,9 @@ extension DJIGimbal {
     
     /// If PitchRange extended, the gimbal's pitch control range
     /// can be [-30, 90], otherwise, it's [0, 90].
-    public func rotate(pitch: Double, time: TimeInterval = 0.0, mode: DJIGimbalRotationMode = .absoluteAngle) -> Promise<Void> {
+    public func rotate(pitch: Double?, roll: Double?, yaw: Double?, time: TimeInterval = 0.0, mode: DJIGimbalRotationMode = .absoluteAngle) -> Promise<Void> {
         return Promise { seal in
-            let rotation = DJIGimbalRotation(pitchValue: NSNumber(value: pitch), rollValue: nil, yawValue: nil, time: time, mode: mode)
+            let rotation = DJIGimbalRotation(pitchValue: pitch.toNSNumber, rollValue: roll.toNSNumber, yawValue: yaw.toNSNumber, time: time, mode: mode)
             self.rotate(with: rotation) { (error) in
                 seal.resolve((), error)
             }
@@ -38,6 +38,16 @@ extension DJIGimbal {
             self.setPitchRangeExtensionEnabled(isEnabled) { (error) in
                 seal.resolve((), error)
             }
+        }
+    }
+}
+
+private extension Optional where Wrapped == Double {
+    var toNSNumber: NSNumber? {
+        if let self = self {
+            return NSNumber(value: self)
+        } else {
+            return nil
         }
     }
 }
