efda3bd6e16d55257689667aa36338ce4dabdb77
Kyle <lacklock@gmail.com>
2019-01-07 18:50:23 +0800

    add startMission func

--- DJISDKExtension/DJIExtraWaypointMission.swift
+++ DJISDKExtension/DJIExtraWaypointMission.swift
@@ -7,13 +7,29 @@
 //
 
 import DJISDK
+import PromiseKit
+
+public protocol DJIExtraWaypointMissionDelegate: class {
+
+}
+
+public enum DJIExtraWaypointMissionError: Error {
+    case getMissionOperatorFailed
+}
 
 /// Waypoint mission with no limit waypoints
 public class DJIExtraWaypointMission: DJIMission {
     
+    public weak var delegate: DJIExtraWaypointMissionDelegate?
+    
     var configuredMission: DJIWaypointMission
     let orignalWaypoints: [DJIWaypoint]
     public let waypointMissions: [DJIWaypointMission]
+    
+    var waypointIndex = 0
+    var missionIndex = 0
+    
+    private let listenerQueue = DispatchQueue(label: "com.kiwiinc.MissionListenerQueue")
     
     ///  Initialize a `DJIExtraWaypointMission` by copying the parameters from the input mission.
     public init(mission: DJIWaypointMission, waypoints: [DJIWaypoint]) throws {
@@ -35,6 +51,48 @@ public class DJIExtraWaypointMission: DJIMission {
             }
         }
         super.init()
+    }
+    
+    public func startMission() -> Promise<Void> {
+        waypointIndex = 0
+        missionIndex = 0
+        return startExecute(mission: waypointMissions[0])
+    }
+    
+    func startExecute(mission: DJIWaypointMission) -> Promise<Void> {
+        guard let missionOperator = DJISDKManager.missionControl()?.waypointMissionOperator() else {
+            return Promise(error: DJIExtraWaypointMissionError.getMissionOperatorFailed)
+        }
+        if let loadError = missionOperator.load(mission) {
+            return Promise(error: loadError)
+        }
+        return Promise {
+            missionOperator.uploadMission(completion: $0.resolve)
+        }.then {
+            self.missionIsReadyToExecute(missionOperator)
+        }.then {
+            Promise {
+                missionOperator.startMission(completion: $0.resolve)
+            }
+        }
+    }
+    
+    private func missionIsReadyToExecute(_ missionOperator: DJIWaypointMissionOperator) -> Promise<Void> {
+        missionOperator.removeListener(ofUploadEvents: self)
+        return Promise { seal in
+            missionOperator.addListener(toUploadEvent: self, with: listenerQueue) { (uploadEvent) in
+                if uploadEvent.currentState == .readyToExecute {
+                    seal.fulfill()
+                    missionOperator.removeListener(ofUploadEvents: self)
+                }
+            }
+        }
+    }
+    
+    deinit {
+        if let missionOperator = DJISDKManager.missionControl()?.waypointMissionOperator() {
+            missionOperator.removeListener(ofUploadEvents: self)
+        }
     }
     
     static func groupWaypoints(_ waypoints: [DJIWaypoint]) -> [[DJIWaypoint]] {
